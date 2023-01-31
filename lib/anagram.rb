9d12bd98ed73a32f60cd15313666c0ea15cfbfac
Allen Kim <avalmercafe02@gmail.com>
2023-01-30 21:54:23 -0500

    commit tree e56092a57cc044437b2c94781a2f5fccfbff0728

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram 
+    attr_accessor :word
+    def initialize (word)
+        @word = word 
+    end 
+    def match(array)
+        array.map do |anagram| 
+           sorted_words = anagram.chars.sort
+        end 
+        if (sorted_words == @word.chars.sort) 
+             sorted_words.join
+        elsif 
+            []
+          end 
+       
+          
+    end 
+end 
